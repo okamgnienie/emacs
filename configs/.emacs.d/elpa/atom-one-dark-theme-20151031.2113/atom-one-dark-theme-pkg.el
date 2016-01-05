13730d4487b4361070ace9a452d55bb4fc54dc76
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-01-05 08:34:58 +0100

    Updated packages.

--- configs/.emacs.d/elpa/atom-one-dark-theme-20151031.2113/atom-one-dark-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "atom-one-dark-theme" "20151031.2113" "Atom One Dark color theme" 'nil :url "https://github.com/jonathanchu/atom-one-dark-theme")
---
+++ configs/.emacs.d/elpa/dash-20151216.1315/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22155 29150 884253 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
