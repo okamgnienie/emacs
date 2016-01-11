89daf3a55831e707f691e5b77a203e57bede1d2b
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-01-11 11:06:05 +0100

    Added smart-forward.

---
+++ configs/.emacs.d/elpa/smart-forward-20140430.13/smart-forward-autoloads.el
@@ -0,0 +1,16 @@
+;;; smart-forward-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("smart-forward.el") (22163 31940 790263
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; smart-forward-autoloads.el ends here
