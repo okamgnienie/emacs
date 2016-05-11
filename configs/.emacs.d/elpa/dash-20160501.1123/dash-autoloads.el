11fd274727902f2d0cee84f4833c65899ccc55f6
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-05-11 10:04:58 +0200

    Updated packages.

--- configs/.emacs.d/elpa/dash-20160501.1123/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash.el") (22312 27489 847938 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
