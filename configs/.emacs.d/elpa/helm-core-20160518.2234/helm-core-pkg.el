536e8fb78a32249e59143dfbc359336df7cff620
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-06-01 10:18:26 +0200

    Updated packages.

--- configs/.emacs.d/elpa/helm-core-20160518.2234/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20160518.2234" "Development files for Helm"
-  '((emacs "24.3")
-    (async "1.8"))
-  :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
