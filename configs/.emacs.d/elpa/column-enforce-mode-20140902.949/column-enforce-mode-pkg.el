6910d99338453f843ebb186161363c9feaadb224
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2016-10-27 21:53:01 +0200

    Update packages

--- configs/.emacs.d/elpa/column-enforce-mode-20140902.949/column-enforce-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "column-enforce-mode" "20140902.949" "Highlight text that extends beyond a  column" 'nil :url "www.github.com/jordonbiondo/column-enforce-mode")
--- configs/.emacs.d/elpa/dash-20160820.501/dash-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (22477 7809 388391
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
