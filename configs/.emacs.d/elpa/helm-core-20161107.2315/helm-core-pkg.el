308ee73b667d3a6a4d4618175484a94cf3e199b9
Przemyslaw Hardyn <przemyslaw.hardyn@cern.ch>
2016-11-14 09:52:16 +0100

    Update packages

--- configs/.emacs.d/elpa/helm-core-20161107.2315/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20161107.2315" "Development files for Helm"
-  '((emacs "24.4")
-    (async "1.9"))
-  :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
