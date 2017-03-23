48a622d639b9bdd53c06fa79640650f190d4360c
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-03-23 14:16:29 +0100

    Update packages

--- configs/.emacs.d/elpa/json-mode-0.1/json-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "json-mode" "0.1" "Major mode for editing JSON files" '((emacs "25.1")) :url "http://elpa.gnu.org/packages/json-mode.html" :keywords '("data"))
--- configs/.emacs.d/elpa/parinfer-20170126.2111/parinfer-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "parinfer" "20170126.2111" "Simpler Lisp editing"
-  '((dash "2.13.0")
-    (cl-lib "0.5"))
-  :url "https://github.com/DogLooksGood/parinfer-mode" :keywords
-  '("parinfer"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
