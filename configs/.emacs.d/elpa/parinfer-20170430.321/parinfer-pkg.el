13769d1220658ef2cb0e4bad8a7972d0f2cfa0e7
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-06-21 20:57:28 +0200

    Update packages

--- configs/.emacs.d/elpa/parinfer-20170430.321/parinfer-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "parinfer" "20170430.321" "Simpler Lisp editing"
-  '((dash "2.13.0")
-    (cl-lib "0.5"))
-  :url "https://github.com/DogLooksGood/parinfer-mode" :keywords
-  '("parinfer"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
